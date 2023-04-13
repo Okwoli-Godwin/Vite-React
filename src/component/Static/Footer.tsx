28f957807ad3169b9b6bac758a163d2ecf8cc1cd
Okwoli-Godwin <okwolig60@gmail.com>
2023-04-13 15:29:54 +0100

    updated

---
+++ src/component/Static/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Footer = () => {
+  return (
+      <Container>
+          
+    </Container>
+  )
+}
+
+export default Footer
+
+const Container = styled.div`
+    width: 100%;
+    height: 200px;
+    background-color: green;
+    color: white;
+`
